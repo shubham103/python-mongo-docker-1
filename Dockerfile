15a179320845ca05b76bb27b774adca1e7e5100d
pelow <pelowif150@eroyal.net>
2020-07-09 07:37:22 +0530

    final commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3
+
+RUN pip install flask
+RUN pip install pymongo
+
+WORKDIR /usr/src/app
+
+COPY app.py .
+
+EXPOSE 5000a
---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from pymongo import MongoClient
+import json
+
+app = Flask(__name__)
+
+client = MongoClient('172.17.0.2',27017)
+
+db = client.shubham
+
+col= db.skt
+
+names= col.find()
+
+@app.route("/",methods=['GET'])
+def index():
+  documents = col.find()
+  response = []
+  for document in documents:
+    document['_id'] = str(document['_id'])
+    response.append(document)
+  return json.dumps(response)
+ 
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0")
+
